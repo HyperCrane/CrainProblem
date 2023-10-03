d9675249f4766ddb8f548710e99f01cdadcfcb89
Hyun <53088084+gusdndl@users.noreply.github.com>
2023-10-03 20:50:51 +0900

    Update a.c

--- Procedural Programming/a.c
+++ Procedural Programming/a.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MAX_COMMANDS 100
+#define MAX_COMMAND_LENGTH 30
+
+char commands[MAX_COMMANDS][MAX_COMMAND_LENGTH];
+int top = 0;
+
 typedef struct {
     int id;
     int status; // 0: inactive, 1: active
@@ -17,88 +23,108 @@ void initialize_motors() {
 }
 
 void activate_motor(int id) {
-    if (motors[id-1].status == 0) {
+    if (motors[id - 1].status == 0) {
         printf("Motor %d: active\n", id);
-        motors[id-1].status = 1;
+        motors[id - 1].status = 1;
     }
 }
 
 void deactivate_motor(int id) {
-    if (motors[id-1].status == 1) {
+    if (motors[id - 1].status == 1) {
         printf("Motor %d: inactive\n", id);
-        motors[id-1].status = 0;
+        motors[id - 1].status = 0;
     }
 }
 
 void right() {
-    activate_motor(1); // x axis motor
+    activate_motor(1); // x-axis motor
 }
 
-void left() { 
-   activate_motor(2); // y axis motor 
+void left() {
+    activate_motor(2); // y-axis motor 
 }
 
-void up() { 
-   deactivate_motor(3); // claw up/down motor 
+void up() {
+    deactivate_motor(3); // claw up/down motor 
 }
 
-void down() { 
-   activate_motor(3); // claw up/down motor  
+void down() {
+    activate_motor(3); // claw up/down motor  
 }
-   
-void grab() {   
-   for (int i=4;i<=8;i++){  
-       activate_motor(i); // fingers' motors
-   }   
-}   
 
-void let_go(){
-     int flag=0;
-     for (int i=4;i<=8;i++){  
-         if(motors[i-1] .status == true){
-             flag=1 ;
-             break ;
-         }
-     }    
-     
-     if(flag==0){
-         printf("I already opened my finger\n");
-         return ;
-     }
+void grab() {
+    for (int i = 4; i <= 8; i++) {
+        activate_motor(i); // fingers' motors
+    }
+}
 
-     for(int i=4;i<=8;i++){
-         deactivate_motor(i);
-     }     
-}   
+void let_go() {
+    int flag = 0;
+    for (int i = 4; i <= 8; i++) {
+        if (motors[i - 1].status == 1) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0) {
+        printf("I already opened my finger\n");
+        return;
+    }
+
+    for (int i = 4; i <= 8; i++) {
+        deactivate_motor(i);
+    }
+}
+
+void list_commands() {
+    printf("Command List:\n");
+    for (int i = 0; i < top; i++) {
+        printf("%d. %s\n", i + 1, commands[i]);
+    }
+}
 
 int main(void) {
     char command[20];
     initialize_motors();
 
-    while(1) {
+    while (1) {
         printf("Enter a command: ");
         fgets(command, 20, stdin);
-        // remove newline character
+        // Remove newline character
         command[strcspn(command, "\n")] = 0;
 
         if (strcmp(command, "right") == 0) {
             right();
-        } else if (strcmp(command, "down") == 0) {
+            strncpy(commands[top++], command, MAX_COMMAND_LENGTH);
+        }
+        else if (strcmp(command, "down") == 0) {
             down();
-        } else if (strcmp(command, "grab") == 0) {
+            strncpy(commands[top++], command, MAX_COMMAND_LENGTH);
+        }
+        else if (strcmp(command, "grab") == 0) {
             grab();
-        } else if (strcmp(command, "up") == 0) {
+            strncpy(commands[top++], command, MAX_COMMAND_LENGTH);
+        }
+        else if (strcmp(command, "up") == 0) {
             up();
-	} else if (strcmp(command,"left")==0){
-	    left();
-	} else if(strcmp(command,"let go")==0){
-	    let_go();
-	} else if(strcmp(command,"exit")==0){
-	     break ;
-	} else{
-	    printf("Invalid command! Please enter again.");
-	}
-   }
+            strncpy(commands[top++], command, MAX_COMMAND_LENGTH);
+        }
+        else if (strcmp(command, "left") == 0) {
+            left();
+            strncpy(commands[top++], command, MAX_COMMAND_LENGTH);
+        }
+        else if (strcmp(command, "let go") == 0) {
+            let_go();
+            strncpy(commands[top++], command, MAX_COMMAND_LENGTH);
+        }
+        else if (strcmp(command, "exit") == 0) {
+            break;
+        }
+        else {
+            printf("Invalid command! Please enter again.");
+        }list_commands();
+    }
 
     return 0;
 }
